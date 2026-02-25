94cf4e7b6e2f3b2e80937060013c4b48dce8f0bc
Alexey Artamonov <darksailas@yandex.ru>
2026-02-25 17:42:47 +0300

    feat: implement Firewall4 (fw4) integration and dnsmasq confdir management

--- podkop/files/usr/lib/nft.sh
+++ podkop/files/usr/lib/nft.sh
@@ -20,6 +20,61 @@ nft_create_ifname_set() {
     nft add set inet "$table" "$name" '{ type ifname; flags interval; }'
 }
 
+# Generates a static nftables ruleset file for fw4 inclusion
+nft_generate_static_ruleset() {
+    local table_name="$1"
+    local interface_set="$2"
+    local common_set="$3"
+    local localv4_set="$4"
+    local fakeip_mark="$5"
+    local outbound_mark="$6"
+    local fakeip_range="$7"
+    local tproxy_port="$8"
+    local exclude_ntp="$9"
+
+    cat <<EOF
+table inet $table_name {
+    set $localv4_set {
+        type ipv4_addr
+        flags interval
+        auto-merge
+        elements = { 0.0.0.0/8, 10.0.0.0/8, 127.0.0.0/8, 169.254.0.0/16, 172.16.0.0/12, 192.0.0.0/24, 192.0.2.0/24, 192.88.99.0/24, 192.168.0.0/16, 198.51.100.0/24, 203.0.113.0/24, 224.0.0.0/4, 240.0.0.0-255.255.255.255 }
+    }
+
+    set $common_set {
+        type ipv4_addr
+        flags interval
+        auto-merge
+    }
+
+    set $interface_set {
+        type ifname
+        flags interval
+    }
+
+    chain mangle {
+        type filter hook prerouting priority -150; policy accept;
+        $( [ "$exclude_ntp" = "1" ] && echo "udp dport 123 return" )
+        iifname @$interface_set ip daddr @$common_set meta l4proto { tcp, udp } meta mark set $fakeip_mark counter
+        iifname @$interface_set ip daddr $fakeip_range meta l4proto { tcp, udp } meta mark set $fakeip_mark counter
+    }
+
+    chain mangle_output {
+        type route hook output priority -150; policy accept;
+        ip daddr @$localv4_set return
+        meta mark $outbound_mark counter return
+        ip daddr @$common_set meta l4proto { tcp, udp } meta mark set $fakeip_mark counter
+        ip daddr $fakeip_range meta l4proto { tcp, udp } meta mark set $fakeip_mark counter
+    }
+
+    chain proxy {
+        type filter hook prerouting priority -100; policy accept;
+        meta mark & $fakeip_mark == $fakeip_mark meta l4proto { tcp, udp } tproxy ip to 127.0.0.1:$tproxy_port counter
+    }
+}
+EOF
+}
+
 # Add one or more elements to a set
 nft_add_set_elements() {
     local table="$1"
