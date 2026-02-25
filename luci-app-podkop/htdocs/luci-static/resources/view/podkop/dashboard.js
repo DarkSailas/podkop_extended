bc5ec7fee5b011b6a301f2e411022cddabdea69d
Alexey Artamonov <darksailas@yandex.ru>
2026-02-25 18:15:16 +0300

    \ Refactor: Remove IPv6 plans and implement basic client-side dashboard\

--- luci-app-podkop/htdocs/luci-static/resources/view/podkop/dashboard.js
+++ luci-app-podkop/htdocs/luci-static/resources/view/podkop/dashboard.js
@@ -4,12 +4,40 @@
 "require ui";
 "require uci";
 "require fs";
+"require ui";
+"require rpc";
+
+const callServiceStatus = rpc.declare({
+  object: 'luci.podkop',
+  method: 'get_status',
+  expect: { "": {} }
+});
 
 function createDashboardContent(section) {
   const o = section.option(form.DummyValue, "_mount_node");
   o.rawhtml = true;
-  o.cfgvalue = () => {
-    return _("Dashboard logic is being modernized. Please wait for the next update.");
+  o.cfgvalue = function () {
+    return E('div', { 'class': 'spinning' }, _('Fetching status...'));
+  };
+
+  o.render = function () {
+    return L.resolveDefault(callServiceStatus(), {}).then(status => {
+      return E('div', { 'class': 'cbi-value' }, [
+        E('label', { 'class': 'cbi-value-title' }, _('Service Status')),
+        E('div', { 'class': 'cbi-value-field' }, [
+          E('span', {
+            'class': status.running ? 'label success' : 'label warning',
+            'style': 'margin-right: 10px'
+          }, status.running ? _('Running') : _('Stopped')),
+          E('button', {
+            'class': 'cbi-button cbi-button-action',
+            'click': ui.createHandlerFn(this, () => {
+              return fs.exec('/etc/init.d/podkop', ['restart']).then(() => location.reload());
+            })
+          }, _('Restart Podkop'))
+        ])
+      ]);
+    });
   };
 }
 
